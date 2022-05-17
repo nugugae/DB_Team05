ae8bb944afadb7b6a81b089df075098b7302d4a2
nugugae <89291329+nugugae@users.noreply.github.com>
2022-05-17 11:55:38 +0900

    Add files via upload

---
+++ Login/Login.js
@@ -0,0 +1,18 @@
+
+document.querySelector("#login").addEventListener("click", () => {
+    const id = "admin";
+    const password = "1234";
+ 
+    if(id == document.querySelector("#id").value) {
+        if(password == document.querySelector("#password").value) {
+            alert("환영합니다!");
+            window.location.href = "home.html"; 
+        }
+        else {
+            alert("비밀번호가 맞지 않습니다.");
+        }
+    }
+    else {
+        alert("아이디 혹은 비밀번호가 맞지 않습니다.");
+    }
+});
