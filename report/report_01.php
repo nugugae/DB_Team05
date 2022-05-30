9b914f3f0826a3e93a4aa48b9521c129a3c8f212
jiyoung <jiyoung_06@ewhain.net>
2022-05-30 18:34:30 +0900

    add php files

---
+++ report/report_01.php
@@ -0,0 +1,96 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset="UTF-8">
+	<title>데이터베이스 보고서1</title>
+	<link rel="stylesheet" href="report.css">
+    </head>
+
+    <body>
+    <div id="wrapper">
+    <header id="main_header">
+        <a href="report_DB.html"><h1>DATABASE REPORT</h1></a>
+    </header>  
+    <nav id="main_menu">  
+        <ul>
+            <a href="report_01.html"><li> 보고서 1 </li></a>
+            <a href="report_02.html"><li> 보고서 2 </li></a>
+            <a href="report_03.html"><li> 보고서 3 </li></a>
+        </ul>
+    </nav>
+    
+    <section id="main_section"> 
+
+    <article>  
+    <header>
+        <hgroup>
+            <h1>문제점</h1><br>
+            <h2>식사 후 푸드코트 내 카페의 낮은 이용률</h2><br>
+        </hgroup>
+            <p>
+            푸드 코트 내 음식점을 이용한 사람이 푸드코트 내 카페를 이용하지 않는 경우가 많다. 
+            <br><br>
+            <figure>
+                <image src="./assets/report1-1.jpg" style="width:700px; height: 300px;"></image>
+                <figcaption>카페의 매장 내 이용과 테이크 아웃 비율</figcaption>
+            </figure>
+            <br><br>
+            위의 그래프는 카페 이용자의 매장이용 혹은 테이크아웃 실태를 나타낸 그래프이다. </br>
+	    카페 이용자의 대부분은 테이크아웃 손님이다. 
+	    위의 그래프에서 0은 매장이용, 1은 테이크아웃을 의미하며 테이크아웃 비율을 나타낸 것이다. 
+            약 1/4도 안되는 사람들만이 매장을 이용하고 대부분은 카페 테이크아웃을 즐기는 것을 확인할 수 있다.
+            <br><br>
+	    <figure>
+                <image src="./assets/report1-2.jpg" style="width:400px; height: 300px;"></image>
+                <figcaption>카페 이용시간</figcaption>
+            </figure>
+	    <br><br>
+            위의 그래프는 카페 이용자의 이용시간에 대한 정보를 나타낸 그래프이다. </br>
+	    많은 사람들이 카페를 10시-13시, 16시-19시에 이용하며 이 시간대에 카페 이용률이 높음을 그래프를 통해 확인할 수 있다. 
+            특히 12-13시, 18-19시는 점심, 저녁시간으로 식사시간과 겹친다. 많은 사람들이 식사 후, 카페를 이용하고 있음을 추측할 수 있다.
+            <br><br> 
+	    <figure>
+                <image src="./assets/report1-3.jpg" style="width:400px; height: 300px;"></image>
+                <figcaption>푸드코트 내 다른 음식점을 이용한 사람들과의 관계성</figcaption>
+            </figure>
+	    <br><br>
+            위의 그래프는 카페를 이용하는 사람 중에 푸드코트 음식점을 이용하는 사람이 얼마나 되는지 나타낸 그래프이다. </br>
+	    카페 이용자 21명 중 13명이 푸드코트 내 음식점을 이용하지 않은 사람이었다. 
+            13명이라는 숫자는 21명의 과반수로 카페 이용자의 2/3정도 되는 사람들이 푸드코트 외부에서 밥을 먹은 후 이용하고 있음을 알 수 있다.  
+		    
+            <br><br>
+            </p>
+        
+   </header>
+   
+
+      
+    </article>
+
+    <article>  
+        <hgroup>
+            <h1>해결방안</h1><br>
+            <h2>카페 할인</h2><br>
+            <p>
+	    푸드코트 내 음식점을 이용한 사람들에 한해서 그날 당일 카페 할인권을 지급하는 방식으로 매출을 증대시키고자 한다. </br>
+            새로운 타겟을 정하여 방안을 수립한다면 매출이 오를 수 있을거라 생각했다. 카페 이용자의 대부분이 푸드코트를 이용하지 않은 사람들이었기 때문에 푸드코트를 이용하는 사람들 중에서 카페를 이용하는 사람들이 증가한다면, 카페 매출이 오를 수 있다. 
+	    게다가 연쇄적으로 푸드코트에서 식사를 하면 카페 할인권을 준다고 하면, 카페를 좀 더 저렴하게 이용하기 위해서 푸드코트에서 식사를 하는 사람들도 증가할 수 있다.
+            </p>
+        </hgroup>
+                  
+
+    </article>
+    
+    </section>
+
+    
+     <footer id="main_footer">
+        Copyright &copy; 데베5조_2022_All Rights Reserved.
+    </footer>
+    </div>    
+    </body>
+</html>
+
+ 
